ab0ab3e4d11c29d052f95d9297d91c2170e23ef4
rvsmooth <riveeks.smooth@gmail.com>
2025-12-03 14:57:54 +0530

    build.sh: use a bash array to store packages and bulk install

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,22 +3,6 @@
 set -ouex pipefail
 
 ### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux neovim
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
 # Install VSCode
 dnf5 config-manager addrepo --set=baseurl="https://packages.microsoft.com/yumrepos/vscode" --id="vscode"
 dnf5 config-manager setopt vscode.enabled=0
@@ -26,19 +10,24 @@ dnf5 config-manager setopt vscode.gpgcheck=0
 dnf5 install --nogpgcheck --enable-repo="vscode" -y \
   code
 
-# Install firefox and ublock-origin
-dnf5 install -y firefox mozilla-ublock-origin
+# Install other packages
+PACKAGES=(
+  cargo
+  clang
+  fastfetch
+  firefox
+  gcc-c++
+  gwenview
+  kde-connect
+  libreoffice
+  mozilla-ublock-origin
+  neovim
+  rust
+  rustup
+  tmux
+)
 
-# Install libreoffice
-dnf5 install -y libreoffice
-
-# Install c++ compiler
-dnf install -y gcc-c++
+dnf5 install -y "${PACKAGES[@]}"
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
-
-# disable autoupdates
-systemctl disable uupd.timer uupd.service \
-  rpm-ostreed-automatic.timer rpm-ostreed-automatic.service \
-  flatpak-preinstall.service kwalletd5

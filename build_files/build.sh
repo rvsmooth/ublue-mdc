ae398772153a7f2be41fc782be58c447310faea8
rvsmooth <riveeks.smooth@gmail.com>
2025-12-03 16:54:25 +0530

    chore: install starship from binary and init it

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,8 +13,10 @@ dnf5 install --nogpgcheck --enable-repo="vscode" -y \
 # Install other packages
 PACKAGES=(
   clang
+  curl
   fastfetch
   firefox
+  fish
   gcc-c++
   gwenview
   kde-connect
@@ -22,12 +24,23 @@ PACKAGES=(
   mozilla-ublock-origin
   neovim
   tmux
+  wget
 )
 
 dnf5 install -y "${PACKAGES[@]}"
 
 ### Copy files to system
 rsync -rvKl /ctx/system_files/ /
+
+### Install starship
+curl -L "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz" --retry 3 -o /tmp/starship.tar.gz
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+
+## Add
+echo 'eval "$(starship init bash)"' | sudo tee -a /etc/bashrc
+echo 'eval "$(starship init zsh)"' | sudo tee -a /etc/zshrc
+echo 'starship init fish | source' | sudo tee -a /etc/fish/config.fish
 
 ### Run scripts
 /ctx/build_files/00-branding.sh

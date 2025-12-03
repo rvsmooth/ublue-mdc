24acba0c7df991e40ce3701b67c2b3083b6e8a96
rvsmooth <riveeks.smooth@gmail.com>
2025-12-03 20:12:48 +0530

    chore: add wallpaper config

--- build_files/00-branding.sh
+++ build_files/00-branding.sh
@@ -2,3 +2,4 @@
 
 ln -sf /usr/share/ublue-mdc/wallpaper/wallpaper.jxl /usr/share/backgrounds/default.jxl
 ln -sf /usr/share/ublue-mdc/wallpaper/wallpaper.jxl /usr/share/backgrounds/default-dark.jxl
+ln -sf /usr/share/ublue-mdc/wallpaper/calcifer.xml /usr/share/backgrounds/default.xml
